3b99de0b5bb58933e3cf5a5dda23063583b54d9d
Luis Valencia <luis17sept@gmail.com>
2024-10-13 01:40:26 -0600

    Transiciones y pestaña de experiencia

---
+++ src/lib/AbilitySection.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let title
+    export let abilities
+    export let urlImg
+    let duration = 1000
+</script>
+
+<div class="grid mt-1 sm:grid-cols-5" >
+    <div class="p-4 sm:col-span-2 lg:col-span-1 h-full flex items-center overflow-hidden" data-aos="zoom-in" data-aos-duration="800" data-aos-once="true" >
+        <img class="rounded-lg border-gray-100" src={urlImg} alt="">
+    </div>    
+    <div class="p-4 sm:col-span-3 lg:col-span-4">
+        <h2 class="text-lg font-medium mb-2" data-aos="fade-left" data-aos-duration="800" data-aos-once="true">
+            { title }
+        </h2>
+        <ul class="list-disc ml-6">
+            {#each abilities as ability, i}
+                <li data-aos="fade-left" data-aos-duration="{duration + (i * 100)}" data-aos-once="true">{ ability }</li>
+            {/each}
+        </ul>
+    </div>
+</div>
--- src/lib/AcademicCard.svelte
+++ src/lib/AcademicCard.svelte
@@ -1,42 +1,33 @@
 <script>
-    import { fly } from 'svelte/transition';
     export let title;
     export let university;
     export let university2;
     export let years;
-	let visible = false;
-    
-    setTimeout(() => {
-        visible = true;
-    }, 500);
 
 </script>
 
 <div class="card-space flex justify-center">
-    {#if visible}
-        <div class="card bg-gray-100 w-56 h-72 rounded-xl m-4 shadow-lg transition-transform transform hover:scale-105 will-change-transform"
-            transition:fly={{ y: 200, duration: 2000 }}>
-            <div class="flex p-2 gap-1">
-                <div>
-                    <span class="circle1 inline-block center w-3 h-3 rounded-full"></span>
-                </div>
-                <div class="circle">
-                    <span class="circle2 inline-block center w-3 h-3 rounded-full"></span>
-                </div>
-                <div class="circle">
-                    <span class="circle3 box inline-block center w-3 h-3 rounded-full"></span>
-                </div>
+    <div class="card bg-gray-100 w-56 h-72 rounded-xl m-4 shadow-lg transition-transform transform hover:scale-105 will-change-transform">
+        <div class="flex p-2 gap-1">
+            <div>
+                <span class="circle1 inline-block center w-3 h-3 rounded-full"></span>
             </div>
-            <div class="card-container flex">
-                <div class="card-content flex-grow mx-3 bg-gray-50 rounded-xl">
-                    <h3 class="text-lg font-bold">{title}</h3>
-                    <h3>{university}</h3>
-                    <h3>{university2}</h3>
-                    <p class="subtext text-xs text-gray-500">{years}</p>
-                </div>
+            <div class="circle">
+                <span class="circle2 inline-block center w-3 h-3 rounded-full"></span>
+            </div>
+            <div class="circle">
+                <span class="circle3 box inline-block center w-3 h-3 rounded-full"></span>
             </div>
         </div>
-    {/if}
+        <div class="card-container flex">
+            <div class="card-content flex-grow mx-3 bg-gray-50 rounded-xl">
+                <h3 class="text-lg font-bold">{title}</h3>
+                <h3>{university}</h3>
+                <h3>{university2}</h3>
+                <p class="subtext text-xs text-gray-500">{years}</p>
+            </div>
+        </div>
+    </div>
 </div>
 
 <style>

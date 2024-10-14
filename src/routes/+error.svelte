505cf90e3e2df63500d78534ae306bf5c4c92aa6
Luis Valencia <luis17sept@gmail.com>
2024-10-13 19:45:45 -0600

    Corrección de diseño en página de error 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,15 +1,14 @@
-<main>
-    <div class="container-pers mt-10 grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div class="text-center">
-            <p class="text-status text-base ">404</p>
-            <h1 class="mt-4 text-2xl font-bold tracking-tight text-gray-900 sm:text-5xl">¡Página no encontrada!</h1>
-            <p class="mt-6 text-base leading-7 text-gray-600">Lo sentimos, la página que buscas no existe.</p>
-            <div class="mt-10 flex items-center justify-center gap-x-6">
-                <a href="/" class="button rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Volver al inicio</a>
-            </div>
+
+<div class="container-pers mt-10 w-full grid place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center">
+        <p class="text-status text-base ">404</p>
+        <h1 class="mt-4 text-2xl font-bold tracking-tight text-gray-900 sm:text-5xl">¡Página no encontrada!</h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">Lo sentimos, la página que buscas no existe.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+            <a href="/" class="button rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Volver al inicio</a>
         </div>
     </div>
-</main>
+</div>
 
 <style>
 .container-pers{
